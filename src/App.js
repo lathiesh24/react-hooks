edf8ffa0ec7616dd3e3385bd72e290a5b15a7e24
lathiesh24 <lathumahi24@gmail.com>
2023-07-17 02:53:04 +0530

    recent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(1);
+ 
+  const incrementFunction = ()=>{
+      setCounter(
+        counter + 1
+      )
+  }
+
+  const decrementFunction = ()=>{
+    setCounter(counter -1 )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={decrementFunction}>Decrement</button>
+        {counter}
+         <button onClick={incrementFunction}>Increment</button>
+      </div>
     </div>
   );
 }
